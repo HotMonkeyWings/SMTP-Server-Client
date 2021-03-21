452ecd797e68d31cc58ed08608613f4d63baa7b4
HotMonkeyWings <devsony52@gmail.com>
2021-03-22 00:10:15 +0530

    Replaced Mains

--- packet.c
+++ packet.c
@@ -6,37 +6,36 @@
 
 #define HEADER_SIZE 8
 
-int send_packet(int sockfd, char *data, int width){
+int send_packet(int sockfd, char *data, int length){
     char header[HEADER_SIZE];
     char format[20];
 
     sprintf(format, "%%0%dd", HEADER_SIZE - 1);
-    sprintf(header, format, width);
+    sprintf(header, format, length);
 
     header[HEADER_SIZE - 1] = 0;
 
-    send(sockfd, data, width, 0);
+    send(sockfd, header, HEADER_SIZE, 0);
 
-    return send(sockfd, data, width, 0);
+    return send(sockfd, data, length, 0);
 }
 
 int recv_packet(int sockfd, char **data){
     char header[HEADER_SIZE];
 
     recv(sockfd, header, HEADER_SIZE, 0);
+    int length = -1;
+    sscanf(header, "%d", &length);
 
-    int width = -1;
-    sscanf(header, "%d", &width);
-
-    if (width <= 0){
+    if (length <= 0){
         printf("COMM_ERROR : Invalid length/header format\n");
         return -1;
     }
 
-    char *buffer = calloc(width, sizeof(char));
+    char *buffer = calloc(length, sizeof(char));
     *data = buffer;
 
-    int recv_len = recv(sockfd, buffer, width, 0);
+    int recv_len = recv(sockfd, buffer, length, 0);
     buffer[recv_len] = '\0';
     return recv_len + 1;
 }
--- smtpmail.c
+++ smtpmail.c
@@ -104,6 +104,7 @@ void *handle_client(void *arg){
     Client *client = (Client *)arg;
 
     printf("[+] New client connected!\n");
+    printf("Sockfd:%d\n", client->sockfd);
 
     char *username, *password;
     if (recv_packet(client->sockfd, &username) == -1){
@@ -232,10 +233,12 @@ void *handle_client(void *arg){
     remove_client(client->head, client);
 }
 
+/*
 int main(int argc, char * argv[]){
     
 
     int sockfd, newsockfd, portno, n;
+    int opt = 1;
     char buffer[255];
 
     struct sockaddr_in serv_addr, cli_addr; //Gives internet address 
@@ -244,7 +247,15 @@ int main(int argc, char * argv[]){
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0){
         printf("Error opening Socket");
+        return -1;
     }
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        printf("Error setting socket options!\n");
+        return -1;
+    }
+
 
     bzero((char *) &serv_addr, sizeof(serv_addr)); //Clears the text
 
@@ -256,6 +267,7 @@ int main(int argc, char * argv[]){
 
     if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0){
         printf("Binding Failed.");
+        return -1;
     }
     
     printf("\nListening on Port %d\n\n",portno);
@@ -276,7 +288,7 @@ int main(int argc, char * argv[]){
 
     fseek(acc_file, 0, SEEK_SET);
    
-    int i;
+    int i = 0;
 
     while(fscanf(acc_file, "%[^,]%*c %[^\n]%*c", fusername, fpassword) != EOF){
         strcpy(acc[i].username, fusername);
@@ -303,5 +315,121 @@ int main(int argc, char * argv[]){
         close(newsockfd);
     }
     close(sockfd);
+    return 0;
+}
+void show_usage()
+{
+    printf("Usage: ");
+    printf("./client.o PORT\n");
+    printf("PORT is the port number to use to connect to the SMTP server\n");
+}
+*/
+int main(int argc, char *argv[])
+{
+    int port = PORT;
+    if (argc == 2)
+    {
+        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0)
+        {
+            show_usage();
+            return 0;
+        }
+        if (sscanf(argv[1], "%d", &port) != 1)
+        {
+            printf("Invalid PORT number\n");
+            show_usage();
+            return -1;
+        }
+    }
+    else if (argc == 1)
+    {
+        printf("PORT not specified\n");
+        printf("Using the default port as %d\n", port);
+    }
+    else
+    {
+        printf("Invalid arguments\n");
+        show_usage();
+        return -1;
+    }
+    int server_fd, conn_socket, recv_len;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char message[256];
+    char buffer[1024] = {0};
+
+    printf("Starting SMTP server...\n");
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\nError creating socket\n");
+        return -1;
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
+        printf("Error setting socket options!\n");
+        return -1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        printf("Error binding to port!\n");
+        return -1;
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        printf("Error while listening for connections!\n");
+        return -1;
+    }
+
+    printf("Server started!\n");
+    printf("Listening on 0.0.0.0:%d\n", port);
+
+    char username[50], password[50];
+
+    FILE *cred_file = fopen("logincred.txt", "r");
+
+    char fusername[50], fpassword[50];
+
+    int total = 0;
+    while (fscanf(cred_file, "%[^,]%*c %[^\n]%*c", fusername, fpassword) != EOF)
+        total++;
+
+    Acc *creds = calloc(total, sizeof(Acc));
+
+    fseek(cred_file, 0, SEEK_SET);
+    int i = 0;
+    while (fscanf(cred_file, "%[^,]%*c %[^\n]%*c", fusername, fpassword) != EOF)
+    {
+        strcpy(creds[i].username, fusername);
+        strcpy(creds[i].password, fpassword);
+        i++;
+    }
+
+    Client *head = init_list();
+
+    while (1)
+    {
+        if ((conn_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
+            printf("Error accepting connection!");
+            continue;
+        }
+
+        fflush(stdout);
+
+        Client *client = new_client(conn_socket, head, creds, total);
+        add_client(head, client);
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_client, client);
+    }
+
     return 0;
 }
